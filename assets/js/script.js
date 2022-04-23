40f457cf2e1e534aac99fd66aaf98a1d784e2806
Michael Hancock <shanehancock64@live.com>
2022-04-23 10:39:15 -0600

    last CSS & HTML updates
    styled buttons and search bar. updated header

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,6 +89,7 @@ function handleSearchFormSubmit(event) {
                  (${movieYear})
              </p>
          </header>
+<<<<<<< Updated upstream
          <!-- Modal toggle -->
          <button id="modalBtn" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
            Streaming Sources
@@ -125,6 +126,41 @@ function handleSearchFormSubmit(event) {
                  </div>
              </div>
          </div>
+=======
+    <!-- Search streaming services and Modal toggle button -->
+    <button id=${imdb} type="button"
+  class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
+  data-bs-toggle="modal" data-bs-target="#staticBackdrop${i}">
+  Show Streaming Sources
+</button>
+         
+<!-- Main modal -->
+<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+  id="staticBackdrop${i}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+  aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog relative w-auto pointer-events-none">
+    <div
+      class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+      <div
+        class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+        <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel${i}">
+          Streaming Sources for ${movieTitle}
+        </h5>
+        <button type="button"
+          class="btn-close box-content w-4 h-4 p-1 background-color red text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+          data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div id="links-area${i}" imdb-id="${imdb}" class="modal-body relative p-4"></div>
+      <div
+        class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+        <button type="button"
+          class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"
+          data-bs-dismiss="modal">Close</button>       
+      </div>
+    </div>
+  </div>
+</div>
+>>>>>>> Stashed changes
      </article>
      <!-- END Article -->
 
